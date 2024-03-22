6254f20de512c0e2fe22ddc92fa49089eac8faf9
jtompkin <jtompkin-dev@pm.me>
2024-03-22 17:57:37 -0400

    Add stack and stack_actions

---
+++ src/pyclacker/stack.py
@@ -0,0 +1,150 @@
+from collections.abc import Callable
+from shutil import get_terminal_size
+from textwrap import wrap
+from string import digits
+import sys
+
+try:
+    import pyclacker.stack_actions as sacs
+except ImportError:
+    import stack_actions as sacs
+
+
+class Stack:
+    def __init__(self, initial: list[float] | None = None) -> None:
+        if initial is None:
+            self.stack: list[float] = []
+        else:
+            self.stack = initial
+        self.operators: dict[str, StackOperator | HelpOperator] = {
+            "+": StackOperator(2, 1, sacs.add),
+            "-": StackOperator(2, 1, sacs.subtract),
+            "*": StackOperator(2, 1, sacs.multiply),
+            "/": StackOperator(2, 1, sacs.divide),
+            "^": StackOperator(2, 1, sacs.power),
+            ".": StackOperator(0, 0, sacs.display),
+            ",": StackOperator(1, 0, sacs.pop),
+            "clear": StackOperator(0, 0, sacs.clear),
+            "quit": StackOperator(0, 0, sacs.quit),
+            "words": HelpOperator(_words),
+            "help": HelpOperator(_help),
+        }
+        self.words: dict[str, list[str]] = {
+            "sqrt": ["0.5", "^"],
+            "pi": ["3.141592654"],
+        }
+        self.parse_length: int = 0
+        self.current: int = 0
+
+    def parse_input(self, input_string: str) -> None:
+        words = input_string.strip().split(" ")
+        self.parse_length = len(words)
+        self.current = 0
+        for token in words:
+            if token == "=":
+                self.add_word(words[self.current :])
+                return
+            self._parse_token(token)
+            self.current += 1
+
+    def add_word(self, definition: list[str]) -> bool:
+        """
+        Add word and definition. `definition` should include
+        '=' as first item. Returns `False` if unsuccessful
+        """
+        if len(definition) < 2:
+            return True
+        if len(definition) == 2:
+            self.words.pop(definition[1], None)
+            return True
+        if definition[1] in self.operators or definition[1] in digits:
+            sys.stderr.write(f"Cannot redefine: {definition[1]}")
+            return False
+        self.words.update({definition[1]: definition[2:]})
+        return True
+
+    def _parse_token(self, token: str) -> None:
+        if token in self.words:
+            return self._expand_word(token)
+        try:
+            return self._push_value(float(token))
+        except ValueError:
+            return self._do_operator(token)
+
+    def _expand_word(self, word: str) -> None:
+        for token in self.words[word]:
+            self._parse_token(token)
+            self.current += 1
+
+    def _do_operator(self, token: str) -> None:
+        operator = self.operators.get(token, StackOperator())
+        if len(self.stack) < operator.pops:
+            return
+        if isinstance(operator, StackOperator):
+            self.stack = operator(self.stack)
+            return
+        operator(self)
+
+    def _push_value(self, value: float) -> None:
+        if value.is_integer():
+            self.stack.append(int(value))
+        else:
+            self.stack.append(value)
+        if self.current == self.parse_length - 1:
+            sacs.display(self.stack)
+
+
+class StackOperator:
+    """Operator that operates directly on the stack of a `Stack` instance"""
+
+    def __init__(
+        self,
+        pops: int = 0,
+        pushes: int = 0,
+        action: Callable[[list[float]], list[float]] = sacs.nop,
+    ) -> None:
+        self.pops = pops
+        self.pushes = pushes
+        self.action = action
+
+    def __call__(self, stack: list[float]) -> list[float]:
+        return self.action(stack)
+
+
+class HelpOperator:
+    """
+    Operator that does not operate on the stack,
+    only uses information from a `Stack` instance
+    """
+
+    def __init__(self, action: Callable[[Stack], None]) -> None:
+        self.pops = 0
+        self.pushes = 0
+        self.action = action
+
+    def __call__(self, stack: Stack) -> None:
+        return self.action(stack)
+
+
+def _words(stack: Stack) -> None:
+    """Print all defined words to the screen"""
+    for word in stack.words:
+        print(f"{word}: {' '.join(i for i in stack.words[word])}")
+
+
+def _help(stack: Stack) -> None:
+    """Print information about available operators to the screen"""
+    extra_space = 2
+    max_length = max(len(i) for i in stack.operators) + 10
+    term_width = get_terminal_size()[0] - (max_length + extra_space)
+    for operator in stack.operators:
+        operator_help = f"operator: {operator}"
+        padding = max_length - len(operator_help) + extra_space
+        description_help = f"description: {stack.operators[operator].action.__doc__}"
+        sys.stdout.write(operator_help + " " * padding)
+        for chunk in wrap(
+            description_help,
+            term_width,
+            subsequent_indent=" " * (max_length + extra_space),
+        ):
+            print(chunk)
---
+++ src/pyclacker/stack_actions.py
@@ -0,0 +1,96 @@
+import sys
+from typing import NoReturn
+
+
+def display(stack: list[float]) -> list[float]:
+    """Print the stack to the screen"""
+    print(" ".join(str(i) for i in stack))
+    return stack
+
+
+def add(stack: list[float]) -> list[float]:
+    """Pop 2 values from the stack, add them, and push the result to the stack"""
+    stack.append(stack.pop() + stack.pop())
+    display(stack)
+    return stack
+
+
+def subtract(stack: list[float]) -> list[float]:
+    """Pop 2 values from the stack, subtract the second item popped from the first, and push the result to the stack"""
+    x = stack.pop()
+    y = stack.pop()
+    stack.append(y - x)
+    display(stack)
+    return stack
+
+
+def multiply(stack: list[float]) -> list[float]:
+    """Pop 2 values from the stack, multiply them, and push the result to the stack"""
+    stack.append(stack.pop() * stack.pop())
+    display(stack)
+    return stack
+
+
+def divide(stack: list[float]) -> list[float]:
+    """Pop 2 values from the stack, divide the second value popped by the first, and push the result to the stack"""
+    divisor = stack.pop()
+    dividend = stack.pop()
+    if int(divisor) == 0:
+        return _fail(stack, "Cannot divide by 0", dividend, divisor)
+    stack.append(dividend / divisor)
+    display(stack)
+    return stack
+
+
+def power(stack: list[float]) -> list[float]:
+    """Pop 2 values from the stack, raise the second value popped to the power of the first, and push the result to the stack"""
+    exponent = stack.pop()
+    base = stack.pop()
+    if not float(exponent).is_integer() and base < 0:
+        return _fail(
+            stack,
+            "Negative number cannot be raised to non-integer power",
+            base,
+            exponent,
+        )
+    if exponent < 0 and int(base) == 0:
+        return _fail(stack, "0 Cannot be raised to a negative power")
+    stack.append(base**exponent)
+    display(stack)
+    return stack
+
+
+def pop(stack: list[float]) -> list[float]:
+    """Pop a single value from the stack"""
+    stack.pop()
+    display(stack)
+    return stack
+
+
+def clear(stack: list[float]) -> list[float]:
+    """Clear the entire stack"""
+    print(f"cleared {len(stack)} values")
+    return []
+
+
+def nop(stack: list[float]) -> list[float]:
+    """Do Nothing"""
+    return stack
+
+
+def quit(_: list[float]) -> NoReturn:
+    """Exit interactive mode"""
+    sys.exit(0)
+
+
+def _fail(
+    stack: list[float], message: str, *values: float, push: bool = True
+) -> list[float]:
+    """
+    Called when a computation cannot be done. Prints a message to the screen,
+    and optionally pushes values back onto the stack
+    """
+    if push:
+        stack += values
+    sys.stderr.write(message + "\n")
+    return stack
